e019e3f47094a538728915a252ac7d0c3a301891
cpf <cpf0710@163.com>
2022-03-11 10:11:10 +0800

    首次提交

---
+++ dntd/src/test/java/com/cpf/poo/dntd/DntdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpf.poo.dntd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DntdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
